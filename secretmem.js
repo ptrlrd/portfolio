3878ea8829d70417eed630e89c53544575cc5c42
Peter Lord <im@ptrlrd.com>
2020-09-23 09:15:25 -0700

    test

--- secretmem.js
+++ secretmem.js
@@ -2,20 +2,20 @@
 // xhr.onreadystatechange = function () {
 //   if(xhr.readyState === 4 && xhr.status === 200) {
 //     let employees = JSON.parse(xhr.responseText);
-//     let statusHTML = '<ul class="bulleted">';
-    for (let i=0; i<employees.length; i += 1) {
-      if (employees[i].inoffice === true) {
-        statusHTML += '<li class="in">';
-      } else {
-        statusHTML += '<li class="out">';
-      }
-      statusHTML += employees[i].name;
-      statusHTML += '</li>';
-    }
-    statusHTML += '</ul>';
-    document.getElementById('employeeList').innerHTML = statusHTML;
-  }
-};
+// //     let statusHTML = '<ul class="bulleted">';
+//     for (let i=0; i<employees.length; i += 1) {
+//       if (employees[i].inoffice === true) {
+//         statusHTML += '<li class="in">';
+//       } else {
+//         statusHTML += '<li class="out">';
+//       }
+//       statusHTML += employees[i].name;
+//       statusHTML += '</li>';
+//     }
+//     statusHTML += '</ul>';
+//     document.getElementById('employeeList').innerHTML = statusHTML;
+//   }
+// };
 // xhr.open('GET', '../data/employees.json');
 // xhr.send();
 
