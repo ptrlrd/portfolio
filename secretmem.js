9f57de213a254d65617d52d79f99c4bf5815a402
Peter Lord <im@ptrlrd.com>
2020-09-23 12:36:40 -0700

    officer only

--- secretmem.js
+++ secretmem.js
@@ -1,22 +1,32 @@
 // let xhr = new XMLHttpRequest();
 // xhr.onreadystatechange = function () {
-//   if(xhr.readyState === 4 && xhr.status === 200) {
-//     let employees = JSON.parse(xhr.responseText);
-// //     let statusHTML = '<ul class="bulleted">';
-//     for (let i=0; i<employees.length; i += 1) {
-//       if (employees[i].inoffice === true) {
-//         statusHTML += '<li class="in">';
-//       } else {
-//         statusHTML += '<li class="out">';
-//       }
-//       statusHTML += employees[i].name;
-//       statusHTML += '</li>';
+//     if(xhr.readyState === 4 && xhr.status === 200) {
+//         const memberlist = document.querySelector('.memberlist')
+//         let freecompany = JSON.parse(xhr.responseText);
+        
+//         let fcMember = `<table><tr><th>Display Photo</th><th>Character Name</th><th>Character Rank</th></tr>`;
+        
+//         for (let i = 0; i < freecompany.FreeCompany.ActiveMemberCount; i += 1) {
+//             // console.log(`${freecompany.FreeCompanyMembers[i].Name}`);
+//             fcMember += `Welcome to `;
+//             fcMember += '<tr><td><img src="';
+//             fcMember += freecompany.FreeCompanyMembers[i].Avatar;
+//             fcMember += '"></td>';
+//             fcMember += `<td><a href="https://na.finalfantasyxiv.com/lodestone/character/${freecompany.FreeCompanyMembers[i].ID}" target="_blank" rel="noopener noreferrer">`;
+//             fcMember += freecompany.FreeCompanyMembers[i].Name;
+//             fcMember += `</a></td>`;
+//             fcMember += '<td>';
+//             fcMember += freecompany.FreeCompanyMembers[i].Rank;
+//             fcMember += '</td></tr>';
+//             // console.log(fcMember)
+//             }
+        
+//         fcMember += `</table>`;
+//         console.log(fcMember);
+//         memberlist.innerHTML = fcMember;
 //     }
-//     statusHTML += '</ul>';
-//     document.getElementById('employeeList').innerHTML = statusHTML;
-//   }
 // };
-// xhr.open('GET', '../data/employees.json');
+// xhr.open('GET', 'https://xivapi.com/freecompany/9228157111458900661?data=FCM');
 // xhr.send();
 
 let xhr = new XMLHttpRequest();
@@ -24,22 +34,19 @@ xhr.onreadystatechange = function () {
     if(xhr.readyState === 4 && xhr.status === 200) {
         const memberlist = document.querySelector('.memberlist')
         let freecompany = JSON.parse(xhr.responseText);
-        
         let fcMember = `<table><tr><th>Display Photo</th><th>Character Name</th><th>Character Rank</th></tr>`;
-        
         for (let i = 0; i < freecompany.FreeCompany.ActiveMemberCount; i += 1) {
-            // console.log(`${freecompany.FreeCompanyMembers[i].Name}`);
-            fcMember += '<tr><td><img src="';
-            fcMember += freecompany.FreeCompanyMembers[i].Avatar;
-            fcMember += '"></td>';
-            fcMember += `<td><a href="https://na.finalfantasyxiv.com/lodestone/character/${freecompany.FreeCompanyMembers[i].ID}" target="_blank" rel="noopener noreferrer">`;
-            fcMember += freecompany.FreeCompanyMembers[i].Name;
-            fcMember += `</a></td>`;
-            fcMember += '<td>';
-            fcMember += freecompany.FreeCompanyMembers[i].Rank;
-            fcMember += '</td></tr>';
-            // console.log(fcMember)
-            }
+            if (freecompany.FreeCompanyMembers[i].Rank == "Jr. Officer" || freecompany.FreeCompanyMembers[i].Rank == "Officer" || freecompany.FreeCompanyMembers[i].Rank == "Leader" || freecompany.FreeCompanyMembers[i].ID === 10211535 ) {
+                fcMember += '<tr><td><img src="';
+                fcMember += freecompany.FreeCompanyMembers[i].Avatar;
+                fcMember += '"></td>';
+                fcMember += `<td><a href="https://na.finalfantasyxiv.com/lodestone/character/${freecompany.FreeCompanyMembers[i].ID}" target="_blank" rel="noopener noreferrer">`;
+                fcMember += freecompany.FreeCompanyMembers[i].Name;
+                fcMember += `</a></td>`;
+                fcMember += '<td>';
+                fcMember += freecompany.FreeCompanyMembers[i].Rank;
+                fcMember += '</td></tr>';
+            }};
         
         fcMember += `</table>`;
         console.log(fcMember);
@@ -48,5 +55,3 @@ xhr.onreadystatechange = function () {
 };
 xhr.open('GET', 'https://xivapi.com/freecompany/9228157111458900661?data=FCM');
 xhr.send();
-
-// Path freecompany.FreeCompanyMembers[0].Name
