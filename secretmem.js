92f1f91b5fffed40746dced47e52ead657ab8758
Peter Lord <im@ptrlrd.com>
2020-09-25 18:10:10 -0700

    push

--- secretmem.js
+++ secretmem.js
@@ -21,21 +21,15 @@ function generateMemberList(data) {
         let fcMember = `<table>`;
         for (let i = 0; i < data.FreeCompany.ActiveMemberCount; i += 1) {
             if (data.FreeCompanyMembers[i].Rank == "Jr. Officer" || data.FreeCompanyMembers[i].Rank == "Officer" || data.FreeCompanyMembers[i].Rank == "Leader" || data.FreeCompanyMembers[i].ID === 10211535 ) {
-                 if (  Number.isInteger(i/4) === true) {
-                    fcMember += '<tr><td><img src="';
-                    fcMember += data.FreeCompanyMembers[i].Avatar;
-                    fcMember += '"></td>';
-                    fcMember += `<td><a href="https://na.finalfantasyxiv.com/lodestone/character/${data.FreeCompanyMembers[i].ID}" target="_blank" rel="noopener noreferrer">`;
-                    fcMember += data.FreeCompanyMembers[i].Name;
-                    fcMember += `</a></td>`;
-                 } else {
-                    fcMember += '<td><img src="';
-                    fcMember += data.FreeCompanyMembers[i].Avatar;
-                    fcMember += '"></td>';
-                    fcMember += `<td><a href="https://na.finalfantasyxiv.com/lodestone/character/${data.FreeCompanyMembers[i].ID}" target="_blank" rel="noopener noreferrer">`;
-                    fcMember += data.FreeCompanyMembers[i].Name;
-                    fcMember += `</a></td>`;
-                }
+                if (  Number.isInteger(i/4) === true) { 
+                    fcMember += `</tr><tr>`;
+                }                 
+                fcMember += '<td><img src="';
+                fcMember += data.FreeCompanyMembers[i].Avatar;
+                fcMember += '" style="width:50px; height:50px;"></td>';
+                fcMember += `<td><a href="https://na.finalfantasyxiv.com/lodestone/character/${data.FreeCompanyMembers[i].ID}" target="_blank" rel="noopener noreferrer">`;
+                fcMember += data.FreeCompanyMembers[i].Name;
+                fcMember += `</a></td>`;
             }
         };
             
