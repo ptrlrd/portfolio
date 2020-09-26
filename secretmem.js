67b82ac014d9f35b776c3ed8ae17b2709812e434
Peter Lord <im@ptrlrd.com>
2020-09-25 17:00:16 -0700

    fix

--- secretmem.js
+++ secretmem.js
@@ -43,31 +43,3 @@ function generateMemberList(data) {
         console.log(fcMember);
         memberList.innerHTML = fcMember;
 }
-
-
-/*
-for (let i = 0; i < data.FreeCompany.ActiveMemberCount; i += 1) {
-    if (data.FreeCompanyMembers[i].Rank == "Jr. Officer" || data.FreeCompanyMembers[i].Rank == "Officer" || data.FreeCompanyMembers[i].Rank == "Leader" || data.FreeCompanyMembers[i].ID === 10211535 ) {
-        fcMember += '<td><img src="';
-        fcMember += data.FreeCompanyMembers[i].Avatar;
-        fcMember += '"></td>';
-        fcMember += `<td><a href="https://na.finalfantasyxiv.com/lodestone/character/${data.FreeCompanyMembers[i].ID}" target="_blank" rel="noopener noreferrer">`;
-        fcMember += data.FreeCompanyMembers[i].Name;
-        fcMember += `</a></td>`;
-    } else if ( i && (i%4 === 1) ) {
-        fcMember += `<tr>`;
-    }
-};
-
-
-                if ( ) {
-                    fcMember += `<tr>`;
-                } else {
-                    fcMember += '<td><img src="';
-                    fcMember += data.FreeCompanyMembers[i].Avatar;
-                    fcMember += '"></td>';
-                    fcMember += `<td><a href="https://na.finalfantasyxiv.com/lodestone/character/${data.FreeCompanyMembers[i].ID}" target="_blank" rel="noopener noreferrer">`;
-                    fcMember += data.FreeCompanyMembers[i].Name;
-                    fcMember += `</a></td>`;
-                }
-*/
