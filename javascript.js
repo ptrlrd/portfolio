ce60978886fa586ba8d917e769242c7c8d3deb07
Peter Lord <im@ptrlrd.com>
2020-10-05 15:27:47 -0700

    testing

--- javascript.js
+++ javascript.js
@@ -1,34 +1,63 @@
-function toggle() { /** Target navigation -- open and close it when menu button is clicked **/
+function onClick(){
+	const burger = document.querySelector('.burger-container')
 	const nav = document.querySelector('.nav');
 	const burgerContainer = document.querySelector('.burger-container');
 	const blurMain = document.querySelector('main');
 	const navContainer = document.querySelector('.intro-container');
 	
-	if (nav.classList.contains('hide')) {
-		nav.classList.remove('hide') & burgerContainer.classList.add('sliding-menu-button') & blurMain.classList.add('mainblur') & navContainer.classList.add('mainblur');
-	  } else {
-		nav.classList.add('hide') & burgerContainer.classList.remove('sliding-menu-button') & blurMain.classList.remove('mainblur') & navContainer.classList.remove('mainblur');
-	  }
-  }
+	burger.addEventListener('click', (e) => {
+		if(nav.classList.contains('hide')){
+			console.log(e)
+			nav.classList.contains('hide');
+			burgerContainer.classList.add('sliding-menu-button');
+			blurMain.classList.add('mainblur');
+			navContainer.classList.add('mainblur');
+			nav.classList.remove('hide');
+		} else {
+			nav.classList.add('hide');
+			 burgerContainer.classList.remove('sliding-menu-button');
+			 blurMain.classList.remove('mainblur');navContainer.classList.remove('mainblur');
+			nav.classList.add('hide') & blurMain.classList.remove('mainblur');
+			navContainer.classList.remove('mainblur');
 
-  function hyperlinkToggle() { /** Target navigation and close the menu when a hyperlink is selected **/
-	const nav = document.querySelector('.nav');
-	const blurMain = document.querySelector('main');
-	const navContainer = document.querySelector('.intro-container');
+		}
+	})
+}
+
+
+// function toggle() { /** Target navigation -- open and close it when menu button is clicked **/
+// 	const nav = document.querySelector('.nav');
+// 	const burgerContainer = document.querySelector('.burger-container');
+// 	const blurMain = document.querySelector('main');
+// 	const navContainer = document.querySelector('.intro-container');
 	
-	if (nav.classList.contains('hide')) {
-		nav.classList.remove('hide');
-	  } else {
-		nav.classList.add('hide') & blurMain.classList.remove('mainblur') & navContainer.classList.remove('mainblur');
-	  }
-  }
+// 	if (nav.classList.contains('hide')) {
+// 		nav.classList.remove('hide') & burgerContainer.classList.add('sliding-menu-button') & blurMain.classList.add('mainblur') & navContainer.classList.add('mainblur');
+// 	  } else {
+// 		nav.classList.add('hide') & burgerContainer.classList.remove('sliding-menu-button') & blurMain.classList.remove('mainblur') & navContainer.classList.remove('mainblur');
+// 	  }
+//   }
+
+// function hyperlinkToggle() { /** Target navigation and close the menu when a hyperlink is selected **/
+// 	const nav = document.querySelector('.nav');
+// 	const blurMain = document.querySelector('main');
+// 	const navContainer = document.querySelector('.intro-container');
+	
+// 	if (nav.classList.contains('hide')) {
+// 		nav.classList.remove('hide');
+// 	  } else {
+// 		nav.classList.add('hide') & blurMain.classList.remove('mainblur') & navContainer.classList.remove('mainblur');
+// 	  }
+//   }
+
+
 
 
 const konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]; // Konami Code  
 let konamiPosition = 0; // Set position sequence
 
 document.addEventListener('keydown', (e)=> { // pass through e to function
-konamiPosition =  (e.keyCode === konamiCode[konamiPosition]) ? konamiPosition + 1 : 0; // Ternary operator (?) was used to either reset konamiposition to 1 or default to 0
+konamiPosition = (e.keyCode === konamiCode[konamiPosition]) ? konamiPosition + 1 : 0; // Ternary operator (?) was used to either reset konamiposition to 1 or default to 0
 if (konamiPosition == konamiCode.length) { // compare position with konamicode length
 	let name = prompt("Hello! You're a gamer.. please tell me your name.");
 	alert(`Well, hello there ${name}! You've unlocked the Konami Code, nothing has changed on this website, but thank you for finding this easter egg.`);
