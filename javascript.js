a5f3c7c332d41aaa78b972ba7726b7bfcb7abc89
Peter Lord <im@ptrlrd.com>
2020-10-05 17:24:46 -0700

    Testing

--- javascript.js
+++ javascript.js
@@ -4,6 +4,9 @@ const nav = document.querySelector('.nav');
 const burgerContainer = document.querySelector('.burger-container');
 const blurMain = document.querySelector('main');
 const navContainer = document.querySelector('.intro-container');
+const topBun = document.querySelector('.hamburger-tbun')
+const filling = document.querySelector('.hamburger-filling')
+const bottomBun = document.querySelector('.hamburger-bbun')
 
 burger.addEventListener('click', () => {
 	if(nav.classList.contains('hide')){
@@ -13,13 +16,12 @@ burger.addEventListener('click', () => {
 		nav.classList.remove('hide');
 	} else {
 		nav.classList.add('hide');
-			burgerContainer.classList.remove('sliding-menu-button');
-			blurMain.classList.remove('mainblur');navContainer.classList.remove('mainblur');
+		burgerContainer.classList.remove('sliding-menu-button');
+		blurMain.classList.remove('mainblur');navContainer.classList.remove('mainblur');
 		nav.classList.add('hide') & blurMain.classList.remove('mainblur');
 		navContainer.classList.remove('mainblur');
 		}
-	}
-)
+	})
 
 menuLinks.addEventListener('click', () => {
 	if (nav.classList.contains('hide')) {
@@ -27,6 +29,25 @@ menuLinks.addEventListener('click', () => {
 	  } else {
 		nav.classList.add('hide') & blurMain.classList.remove('mainblur') & navContainer.classList.remove('mainblur');
 	  }
+	})
+
+window.addEventListener('scroll', (e) => {
+	if ( (window.scrollY / .95) >= window.outerHeight ) {
+		console.log(scrollY)
+		topBun.classList.remove('burger-yum');
+		topBun.classList.add('burger-burnt');
+		filling.classList.remove('burger-yum');
+		filling.classList.add('burger-burnt');
+		bottomBun.classList.remove('burger-yum');
+		bottomBun.classList.add('burger-burnt');
+	} else {
+		topBun.classList.add('burger-yum');
+		topBun.classList.remove('burger-burnt')
+		filling.classList.add('burger-yum');
+		filling.classList.remove('burger-burnt')
+		bottomBun.classList.add('burger-yum');
+		bottomBun.classList.remove('burger-burnt')
+	}
 })
 
 const konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]; // Konami Code  
